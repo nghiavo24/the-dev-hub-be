99af7bad49ed43920a31495e437ceb02fb3f41bd
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-12-26 15:24:00 -0500

    finish the put and delete for the Controllers

--- controllers/postingController.js
+++ controllers/postingController.js
@@ -15,3 +15,27 @@ router.get('/:id', async (req, res, next) => {
     }
 });
 
+router.post('/', async (req, res) => {
+    const newPosting = await Posting.create(req.body)
+    res.status(201).json(newPosting)
+})
+
+router.put('/:id', (req, res) => {
+    Posting.findOneAndUpdate({_id: req.params.id}, req.body, {
+        new: true,
+    }).then((updatePosting) => {
+        Posting.find({}).then((postings) => {
+            res.json(postings)
+        })
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    Posting.findOneAndDelete({
+        _id: req.params.id,
+    }).then((deletePosting) => {
+        Posting.find({}).then((postings) => {
+            res.json(postings)
+        })
+    })
+})
