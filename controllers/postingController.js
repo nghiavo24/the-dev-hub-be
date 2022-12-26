1c84b2dda4e0140d459512c417ed4de5b4d67417
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-12-26 15:13:41 -0500

    add to get by ID for controller

--- controllers/postingController.js
+++ controllers/postingController.js
@@ -5,3 +5,13 @@ const Posting = require('../models/Posting');
 router.get('/', (req, res) => {
     Posting.find().then((postings) => res.json(postings));
 });
+
+router.get('/:id', async (req, res, next) => {
+    try {
+        const postingId = await Posting.findById(req.params.id);
+        res.json(postingId);
+    } catch (err) {
+        next(err);
+    }
+});
+
