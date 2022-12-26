b6feb81c81d4f3570b0da6431e7cc24ba95387cb
Jeffrey Koshy <koshy.jeffrey57@gmail.com>
2022-12-26 12:18:57 -0500

    add the Application schema

---
+++ models/Applications.js
@@ -0,0 +1,38 @@
+const mongoose = require('../db/connection')
+
+const Schema = mongoose.Schema;
+
+const ApplicationSchema = new Schema(
+    {
+        title:{
+            type: String,
+            required: true,
+            maxLength: 100, 
+        },
+        company: {
+            type: String,
+            required: true,
+            maxLength: 50
+        },
+        applied:{
+            type: Date,
+            default: Date.now
+        },
+        hiring_manager:{
+            type: String,
+            maxLength: 50
+        },
+        compensation: Number,
+        work_site: {
+            type: String,
+            maxLength: 50
+        },
+        location: {
+            type: String,
+            maxLength: 50 
+        },
+        url: String
+    }
+)
+
+module.exports = mongoose.model('Application', ApplicationSchema)
