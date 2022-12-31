5925ab248d675a102c4a8a379c3f1db206082ffb
nghiavo24 <nghia.vo1452@gmail.com>
2022-12-30 20:10:03 -0600

    Fixed redirected route

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.redirect('/thedevhub')
+    res.redirect('/api/thedevhub')
 })
 
 const applicationController = require('./controllers/applicationController');
